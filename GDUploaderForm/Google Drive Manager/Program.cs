4525e6f8032b50571f09f6facc718b8dbe8e8d59
Ioannis Alimpertis <ialimpertis@outlook.com>
2017-03-19 01:21:24 +0200

    Console Application project added to solution packages for google drive api and json files added to project global classes added to console project

---
+++ GDUploaderForm/Google Drive Manager/Program.cs
@@ -0,0 +1,56 @@
+﻿using GDUploaderForm;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google_Drive_Manager
+{
+    class Program
+    {
+        public static List<User> UserList = new List<User>();
+        static string savePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\BackUpManager";
+        static string saveFile = savePath + "\\GDASaves.json";
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        private void loadUsers(string savePath, string saveFile)
+        {
+            Directory.CreateDirectory(savePath);
+            try
+            {
+                if (System.IO.File.Exists(saveFile))
+                {
+                    UserList.Clear();
+                    UserList = JsonConvert.DeserializeObject<List<User>>(System.IO.File.ReadAllText(saveFile));
+
+
+                }
+                else
+                {
+                    using (System.IO.FileStream fs = System.IO.File.Create(saveFile))
+                    {
+                        for (byte i = 0; i < 100; i++)
+                        {
+                            fs.WriteByte(i);
+                        }
+                    }
+
+                    System.IO.File.WriteAllText(saveFile, "[ ]");
+                    loadUsers(savePath, saveFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+
+            }
+        }
+    }
+}
